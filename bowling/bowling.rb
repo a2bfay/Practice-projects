b72e0ae94037180250a2aec2b8a99b4abe9be840
a2bfay <a2bfay@users.noreply.github.com>
2014-09-22 21:36:51 -0400

    substantial changes to sc_pr; roll temp rigged

--- bowling/bowling.rb
+++ bowling/bowling.rb
@@ -26,10 +26,11 @@ def roll					                    # add player as argument later
 	
   if @roll_type == 4					# only for 10th/3rd
     roll_results(2)
+    # score_progress        # # # # # # PENDING
     @frame_no += 1
 		
   elsif @roll_type == 3					# only for 1st bonus after 10th frame strike, so array index always =1
-    roll_results(1)			        # --> means always going to have 3rd roll
+    roll_results(1)			        # --> means always have 3rd roll; don't score
       if @pins_remaining == 0
         @pins_remaining == 10
       end		
@@ -69,7 +70,10 @@ end
 
 
 def roll_results(index)						# requires roll method written in terms of @pins_remaining, not pins_hit (which is now local)
-  pins_hit = rand(0..@pins_remaining)    
+  pins_hit = rand(0..(2 * @pins_remaining))    
+  if pins_hit >= @pins_remaining
+    pins_hit = @pins_remaining
+  end
   @pins_remaining = @pins_remaining - pins_hit
     
   if index == 0 
@@ -85,89 +89,134 @@ end
 # for now, plan on calling w/in roll as score_progress(@frame_no - 1)
 #
 def score_progress(i)
-  puts @frame_scores[i].inspect
-  puts "test #{i}"
-
+  print "\n", @frame_scores[i].inspect, "\t\t\t", @game_scores.inspect, "\n"
+  puts "sp#{i}"
+ 
   framesum = @frame_scores[i].reduce(:+)
   
-  # okay, so: 3-frame scan limit only works if check update frame *first* -
+  
+  # okay, so: 3-frame scan limit only works if check back frames *first* -
   # b/c nil two frames back *always* means consecutive strikes preceding
   #
   if i == 2 && @game_scores[-2].nil? == true
     @game_scores[-2] = 20 + @frame_scores[i][0]
+    puts "line #{__LINE__} 2prev:\t#{@game_scores.inspect}"
   elsif i >= 3 && @game_scores[-2].nil? == true
     @game_scores[-2] = @game_scores[-3] + 20 + @frame_scores[i][0]
+    puts "line #{__LINE__} 2prev:\t#{@game_scores.inspect}"
   end
 
+  
   # now handle bonus on prev frame (only)
+  # can't be completed as number if strike followed by strike  -?-  but only matters early on, otherwise caught by usual bonus method?
+  # or is the point here just to deal with early cases?
   #
-  if i == 1 && @game_scores[-1].nil? == true
+  if i == 1 && @game_scores[0].nil? == true
+  
+    # strike on first frame
+    #
     if @frame_scores[0][0] == 10
       if @frame_scores[1][0] == 10
-        @game_scores << nil
+        @game_scores << nil       # might need this after all
+        puts "line #{__LINE__} bonus - two opening strikes"
+        
+        print "\t*", @frame_scores[i].inspect, "\t", @game_scores[i].inspect, "\t", @game_scores.inspect, "\n\n"
+        return
+        
       else
-        @game_scores[-1] = 10 + framesum
+        @game_scores[0] = 10 + framesum
+        puts "line #{__LINE__} - strike on first frame"
       end
+    # spare on first frame
+    #
     else
-      @game_scores[-1] 
+      @game_scores[0] = 10 + @frame_scores[1][0]
+      puts "line #{__LINE__} - first frame spare"
     end
-  elsif @game_scores[-1].nil? == true
   
-  end
-  
-  #      RESUME HERE
-  
+  elsif i >= 2 && @game_scores[-1].nil? == true         # this still needs cases - not dealing with consec strikes
     
-    # is this frame a bonus? if so, need nil as placeholder?
-    # *all* other cases assume frame tot < 10     --> except 10th frame <--
+    # strike on prev frame
     #
-  if framesum == 10
-    @game_scores << nil
-    
-    # *all* cases below include frame tot < 10
-    # is it the first frame? if so, frametot-->scoretot
-    # don't have to worry about going past beginning of array
+    if @frame_scores[i - 1][0] == 10
+      if @frame_scores[i][0] == 10 
+        @game_scores << nil
+        puts "line #{__LINE__} - two consec strikes"
+        
+        print "\t*", @frame_scores[i].inspect, "\t", @game_scores[i].inspect, "\t", @game_scores.inspect, "\n\n"
+        return
+        
+      else 
+        @game_scores[-1] = @game_scores[-2] + 10 + framesum
+        print "line #{__LINE__} - strike on prev frame: #{@game_scores.inspect}\n"
+        # @game_scores << @game_scores[-1] + framesum   # leave this out for now
+      end
+    # spare on prev frame
     #
-  elsif i == 0
-    @game_scores << framesum
-    
-    # if prev frame has total - no bonus in effect - add current to previous	
-    #
-  elsif @game_scores[-1].nil? == false
-    @game_scores << @game_scores[i - 1] + framesum
-
-  else  
-      # remaining cases all assume nil/bonus in prev frame:
-      # don't need to recheck, but type of bonus matters; 
-      # so does 2prev frame (in every subcase? -- no)
-      #
-
-      # start here b/c two nils in a row can *only* happen with two strikes
-      # gaaaah this will require an exception for early frames, won't it?  (YEP)
-      #
-    if @game_scores[-2].nil? == true
-      print "+ + #{@game_scores[-2]}, #{@game_scores[-3]}, #{@frame_scores[i][0]}\n"
-      @game_scores[-2] = @game_scores[-3] + 20 + @frame_scores[i][0]
-      @game_scores[-1] = @game_scores[-2] + framesum
-      @game_scores << @game_scores[-1] + framesum
-       #
-       # down to either strike or spare on prev frame
-
-       # treat strike next
-       #
-    elsif @frame_scores[i - 1][0] == 10 
-      print "* * #{@game_scores[-1]}, #{framesum}\n"
-      @game_scores[-1] = @game_scores[-2] + 10 + framesum
-      @game_scores << @game_scores[-1] + framesum
-            
-      # leaves spare on prev frame
-      #
     else
       @game_scores[-1] = @game_scores[-2] + 10 + @frame_scores[i][0]
-      @game_scores << @game_scores[-1] + framesum
+      # @game_scores << @game_scores[-1] + framesum
+      puts "line #{__LINE__}: spare prev"
     end
+      
   end
-  print @frame_scores[i].inspect, "\t", @game_scores[i].inspect, "\n"
+  
+  
+  # is this frame a bonus? if so, append nil as placeholder
+  # *all* other cases assume frame tot < 10     --> except 10th frame <--
+  #
+  if framesum == 10
+    @game_scores << nil
+    puts "line #{__LINE__} - nil for 10 frame"
+    
+  # *all* cases below include frame tot < 10
+  # is it the first frame? if so, frametot-->scoretot
+  #
+  elsif i == 0
+    @game_scores << framesum
+    puts "line #{__LINE__} - first frame != 10"
+    
+  # if prev frame has total - no bonus in effect - add current to previous	
+  #
+  elsif @game_scores[-1].nil? == false
+    @game_scores << @game_scores[i - 1] + framesum
+    puts "line #{__LINE__} regular addition for frame>=2"
+
+  else  
+      # # remaining cases all assume nil/bonus in prev frame:
+      # # don't need to recheck, but type of bonus matters; 
+      # # so does 2prev frame (in every subcase? -- no)
+      # #
+
+      # # start here b/c two nils in a row can *only* happen with two strikes
+      # # gaaaah this will require an exception for early frames, won't it?  (YEP)
+      # #
+      # # okay -- yes : this is getting called when only 2nd frame (i=1)  -->> spare in first frame sends here; try extra conditional
+      # #
+    # if i >=2 && @game_scores[-2].nil? == true
+      # print "line #{__LINE__}: #{@game_scores[-2]}, #{@game_scores[-3]}, #{@frame_scores[i][0]} - should no longer see this\n"
+      # @game_scores[-2] = @game_scores[-3] + 20 + @frame_scores[i][0]
+      # @game_scores[-1] = @game_scores[-2] + framesum
+      # @game_scores << @game_scores[-1] + framesum
+       # #
+       # # down to either strike or spare on prev frame
+
+       # # treat strike next
+       # #
+    # elsif @frame_scores[i - 1][0] == 10 
+      # print "line #{__LINE__}: #{@game_scores[-1]}, #{framesum} - strike on prev frame\n"
+      # @game_scores[-1] = @game_scores[-2] + 10 + framesum
+      # @game_scores << @game_scores[-1] + framesum
+            
+      # # leaves spare on prev frame
+      # #
+    # else
+      # @game_scores[-1] = @game_scores[-2] + 10 + @frame_scores[i][0]    # ERROR WHEN CALLED ON 2ND FRAME
+      # @game_scores << @game_scores[-1] + framesum
+      # puts "line #{__LINE__}: spare prev at end of method"
+    # end
+  end
+  print "\t", @frame_scores[i].inspect, "\t", @game_scores[i].inspect, "\t", @game_scores.inspect, "\n\n"
 end
 
 
@@ -177,6 +226,8 @@ end
 def score_complete(i)		
   if i == 9								# last frame
     frame_score = @frame_scores[i].reduce(:+)
+    puts frame_score
+    puts @game_scores[-1]
     @game_scores << @game_scores[-1] + frame_score
 		
   elsif i == 8 && @frame_scores[i][0] == 10		# strike in 9th frame ->>  take first two of 10th
@@ -223,15 +274,18 @@ end
   until @frame_no == 11
     roll	
   end
+  puts
   puts @frame_scores.inspect
+  puts
   
-  # setting aside while writing inline scoring
-  #
-  # 10.times do |i| 
-    # # puts i		# yes, from 0 to 9
-	# score_complete(i) 
-    # print @frame_scores[i].inspect, "\t", @game_scores[i].inspect, "\n"
-  # end
+
+@game_scores = Array.new    # reset for second method
+
+  10.times do |i| 
+  # puts i		# yes, from 0 to 9
+	score_complete(i) 
+    print @frame_scores[i].inspect, "\t", @game_scores[i].inspect, "\t", @game_scores[-1].inspect, "\n"
+  end
 
 # if @game_scores[-1] == 300		# um, don't run in this form unless you mean it
 #   perfect = true
